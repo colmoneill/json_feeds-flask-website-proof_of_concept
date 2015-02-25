3db085c8b54515feb2576621f814fc59dfeb896e
colm <mail@colm.be>
2015-02-25 07:56:08 +0100

    Comments being outputted

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, redirect, render_template, url_for
 from flask.views import MethodView
 from blog.models import Post, Comment
+from flask.ext.mongoengine.wtf import model_form
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
@@ -12,9 +13,37 @@ class ListView(MethodView):
 
 class DetailView(MethodView):
 
-	def get(self, slug):
+	form = model_form(Comment, exclude=['created_at'])
+
+	def get_context(self, slug):
 		post = Post.objects.get_or_404(slug=slug)
-		return render_template('posts/detail.html', posts=posts)
+		form = self.form(request.form)
+
+		context = {
+			"post": post,
+			"form": form
+		}
+		return context
+
+	def get(self, slug):
+		context = self.get_context(slug)
+		return render_template('posts/detail.html', **context)
+
+	def post(self, slug):
+		context = self.get_context(slug)
+		form = context.get('form')
+
+		if form.validate():
+			comment = Comment()
+			form.populate_obj(comment)
+
+			post = context.get('post')
+			post.comments.append(comment)
+			post.save()
+
+			return redirect(url_for('posts.detail', slug=slug))
+
+		return render_template('posts/detail.html', **context)
 
 #register the URLS
 posts.add_url_rule('/', view_func=ListView.as_view('list'))
